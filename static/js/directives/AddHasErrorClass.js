7be5e6e3fcb48a9724b702093dbb8eec4c180b33
Gervasio Marchand <gmc@gmc.uy>
2016-03-24 09:30:32 -0300

    Avoid showing the fields as having errors on page load

--- static/js/directives/AddHasErrorClass.js
+++ static/js/directives/AddHasErrorClass.js
@@ -2,24 +2,26 @@
     define(
         [],
         function () {
-            var AddHasErrorClass = function () {
+            var AddHasErrorClass = function ($timeout) {
                 // from http://stackoverflow.com/a/21739917/920295
                 return {
                     restrict: "A",
                     link: function (scope, element, attrs, ctrl) {
                         var input = element.find('input[ng-model]');
                         if (input.length) {
-                            scope.$watch(function () {
-                                return input.controller('ngModel').$invalid;
-                            }, function (isInvalid) {
-                                element.toggleClass('has-error', isInvalid);
-                            });
+                            $timeout(function(){
+                                scope.$watch(function () {
+                                    return input.controller('ngModel').$invalid;
+                                }, function (isInvalid) {
+                                    element.toggleClass('has-error', isInvalid);
+                                });
+                            }, 500);
                         }
                     }
                 };
             };
 
-            return [AddHasErrorClass];
+            return ['$timeout', AddHasErrorClass];
         }
     );
 }(define));
